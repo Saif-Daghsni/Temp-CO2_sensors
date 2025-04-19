117a77d58dc8c51042c75ea50b27de9f3deff2a8
Saif-Daghsni <daghsnisaif@gmail.com>
2025-04-19 21:48:58 +0100

    the first try

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,5 +1,5 @@
 /* USER CODE BEGIN Header */
-/**
+/** the first try
   ******************************************************************************
   * @file           : main.c
   * @brief          : Main program body
